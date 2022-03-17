cfa5e548fd6af9261baf6a7d1164206d57fb5230
Mike Vautour <mike@iglab.dev>
2022-03-17 18:13:27 -0400

    exercise 1.4 passing classes to props

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,11 +8,12 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  console.log(props)
   return(
   <div>
-       <Part part={props.part1} />
-       <Part part={props.part2} />
-       <Part part={props.part3} />
+       <Part part={props.parts[0]} />
+       <Part part={props.parts[1]} />
+       <Part part={props.parts[2]} />
   </div>
   )
 }
@@ -37,24 +38,25 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name:  'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name:  'using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name:  'State of a component',
-    exercises: 14
-  }
-
+  const parts = [
+    {
+      name:  'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name:  'using props to pass data',
+      exercises: 7
+    },
+    {
+      name:  'State of a component',
+      exercises: 14
+    }
+  ]
   return(
     <div>
       <Header course={course} />
-      <Content  part1={part1} part2={part2} part3={part3}  />
-      <Total totalEx={part1.exercises + part2.exercises + part3.exercises} />
+      <Content  parts={parts} />
+      <Total parts={parts} />
       
     </div>
   )
