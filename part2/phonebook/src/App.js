cd07ccaf2e50801093b2b2874d33e349872710af
Mike Vautour <mike@iglab.dev>
2022-04-12 23:50:38 -0400

    2_6 The Phonebook step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Numbers = ({persons}) => persons.map(person => <Person key={person.name} person={person} />)
+
+const Person = ({person}) => <p>{person.name}</p>
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const addNumber = (event) => {
+    event.preventDefault()
+    const numObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(numObject))
+    setNewName('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div>
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Numbers persons={persons} />
     </div>
-  );
+    
+  )
 }
 
-export default App;
+export default App
