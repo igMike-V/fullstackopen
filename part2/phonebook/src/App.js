2abea5e958497cfd555b542ec6622d0d105ae255
Mike Vautour <mike@iglab.dev>
2022-04-13 22:18:17 -0400

    2_8 The Phonebook step3

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,24 @@ import { useState } from 'react'
 
 const Numbers = ({persons}) => persons.map(person => <Person key={person.name} person={person} />)
 
-const Person = ({person}) => <p>{person.name}</p>
+const Person = ({person}) => <p>{person.name} {person.number}</p>
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '040-12345467'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addNumber = (event) => {
@@ -19,9 +27,11 @@ const App = () => {
     if(! persons.find(person => person.name === newName )){
       const numObject = {
         name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(numObject))
       setNewName('')
+      setNewNumber('')
     } else {
       alert(`${newName} is already added to phonebook`)
     }
@@ -32,10 +42,9 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addNumber}>
         <div>
-          name: <input 
-            value={newName}
-            onChange={handleNameChange}
-          />
+          name: <input value={newName} onChange={handleNameChange} />
+          <br />
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
