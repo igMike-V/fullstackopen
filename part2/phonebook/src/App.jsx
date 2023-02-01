76f1731f30ba3c622a7c01e803edfeb99f8bd027
Mike Vautour <mike@intergalacticlab.com>
2023-02-01 15:38:51 -0500

    Phonebook exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,21 +4,38 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
+
   const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(newName)
+    setPersons(persons.concat({name: newName}))
+    setNewName('')
+  }
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+
+  }
+
+  const directory = persons.map(person => <p className="person" key={person.name}>{person.name}</p>)
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input onChange={handleInputChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={handleSubmit} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {directory}
+
     </div>
   )
 }
