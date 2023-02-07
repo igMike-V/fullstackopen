6c9facfe04f39860b66b2eb920b3023538a77070
Mike Vautour <mike@intergalacticlab.com>
2023-02-07 14:33:18 -0500

    fixed baseUrl

--- part3/phonebook_p3/src/services/persons.jsx
+++ part3/phonebook_p3/src/services/persons.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 //const baseUrl = 'http://localhost:3001/api/persons'
-const baseUrl = '/api/persons/'
+const baseUrl = '/api/persons'
 
 // Retrieve all entries from the server 
 const  getAll = () => {
