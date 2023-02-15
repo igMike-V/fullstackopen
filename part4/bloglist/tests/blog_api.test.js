bd6edc3a5c18ffcee7259ea91da40fb00fb14a2c
Mike Vautour <mike@intergalacticlab.com>
2023-02-15 10:39:25 -0500

    test for suscessful blog creation

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,8 +2,20 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const api = supertest(app)
+const helper = require('./blog_api_test_helper')
 
-//const Blog = require('../models/blog')
+const Blog = require('../models/blog')
+
+// initialize db with dummy blogs
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog)
+    await blogObject.save()
+  }
+})
+
 
 test('list is returned as json', async () => {
   await api
@@ -12,16 +24,35 @@ test('list is returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('number of items returned is one', async () => {
+test('number of items returned is three', async () => {
   const response = await api.get('/api/blogs/')
-  expect(response.body).toHaveLength(1)
+  expect(response.body).toHaveLength(3)
 })
 
-test.only('unique identifier is named "id"', async () => {
-  const response = await api.get('/api/blogs/63eceac08eeb3238ec5c36c8')
-  expect(response.body.id).toBeDefined()
+test('unique identifier is named "id"', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
 })
 
+test('making an HTTP POST request creates a new blog post', async () => {
+  const newBlog = {
+    author: 'Joe Shmoe',
+    title: 'fake blog',
+    url: 'https://testblog.com'
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDB()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+  const urlValue = blogsAtEnd.map(blog => blog.url)
+  expect(urlValue).toContain('https://testblog.com')
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
