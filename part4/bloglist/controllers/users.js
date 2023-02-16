4dddd184bd958d86f4efd4762b324a6fce8da5eb
Mike Vautour <mike@intergalacticlab.com>
2023-02-16 13:29:59 -0500

    4.16 validation and tests for username and pass

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,6 +4,12 @@ const User = require('../models/user')
 
 usersRouter.post('/', async (req, res) => {
   const { username, name, password } = req.body
+  if (password.length < 3){
+    res.status(400).json({ error: 'Error, invalid password. Passwords must be longer then 2 characters long' })
+  }
+  if (username.length < 3){
+    res.status(400).json({ error: `Error, ${username} is an invalid username. Usernames must be longer then 2 characters long` })
+  }
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
