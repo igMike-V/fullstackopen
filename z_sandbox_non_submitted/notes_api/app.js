f1fbffafa526287f971eafb9367328c5777c9990
Mike Vautour <mike@intergalacticlab.com>
2023-02-16 11:58:29 -0500

    Notes project added user authy

--- z_sandbox_non_submitted/notes_api/app.js
+++ z_sandbox_non_submitted/notes_api/app.js
@@ -3,14 +3,13 @@ const express = require('express')
 require('express-async-errors')
 const app = express()
 const cors = require('cors')
+const usersRouter = require('./controllers/users')
 const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
-console.log(config.MONGODB_URI)
-
-logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -25,6 +24,8 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
+app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
