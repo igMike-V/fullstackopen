295c2e9437ab3040a01574120cc7df854e1eed4a
Mike Vautour <mike@intergalacticlab.com>
2023-03-06 15:44:25 -0500

    6.2 unicafe revisited

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import counterReducer from './reducer'
 
 import { createStore } from 'redux'
 
 const store = createStore(counterReducer)
 
-console.log(store.getState())
+const clickHandler = action => {
+  store.dispatch({ type: action })
+}
 
+const Button = ({ action, clickHandler, label })=> {
+  return (
+    <button className='button' id={`button-${action.toLowerCase()}`} onClick={() => clickHandler(action.toUpperCase())}>
+      {label}
+    </button>
+  )
+}
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className="controls">
+        <Button action='good' label='good' clickHandler={clickHandler} />
+        <Button action='ok' label='ok' clickHandler={clickHandler} />
+        <Button action='bad' label='bad' clickHandler={clickHandler} />
+        <Button action='zero' label='reset stats' clickHandler={clickHandler} />
+      </div>
+      <div className="info">
+        <p className="info-good"> good {store.getState().good} </p>
+        <p className="info-good"> ok {store.getState().ok} </p>
+        <p className="info-good"> bad {store.getState().bad} </p>
+      </div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const renderApp = () => {
+  root.render(<App />)
+}
+renderApp()
+store.subscribe(renderApp)
