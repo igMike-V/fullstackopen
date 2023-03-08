296fca8db83fb1603e72b73eb5ca08c625f5f418
Mike Vautour <mike@intergalacticlab.com>
2023-03-08 12:55:17 -0500

    6.8 moved heading back to app

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -12,7 +12,6 @@ const AnecdoteList = () => {
   return (
 
     <div className="anecdote-list">
-      <h2>Anecdotes</h2>
       {anecdotes.sort((a,b) => b.votes - a.votes).map(anecdote =>
         <div key={anecdote.id}>
           <div>
