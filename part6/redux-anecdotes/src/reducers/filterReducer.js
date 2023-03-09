f5095b7c38c89f48a365017dec64f115390fd13a
Mike Vautour <mike@intergalacticlab.com>
2023-03-09 11:05:07 -0500

    6.10 installed ReduxToolkit replace filter reducer

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,16 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      console.log(action.payload)
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialFilterState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: initialFilterState,
+  reducers: {
+    filterChange(state, action) {
       return action.payload
-    default:
-      return state
-  }
-}
+    }
+  },
+})
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
-
-export default filterReducer
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
--- z_sandbox_non_submitted/redux-notes/src/reducers/noteReducer.test.js
+++ z_sandbox_non_submitted/redux-notes/src/reducers/noteReducer.test.js
@@ -5,12 +5,8 @@ describe('noteReducer', () => {
   test('returns new state with action NEW_NOTE', () => {
     const state = []
     const action = {
-      type: 'NEW_NOTE',
-      payload: {
-        content: 'the app state is in redux store',
-        important: true,
-        id: 1
-      }
+      type: 'notes/createNote',
+      payload: 'the app state is in redux store',
     }
 
     deepFreeze(state)
@@ -28,8 +24,7 @@ describe('noteReducer', () => {
         id: 1
       },
       {
-        content: 'state changes are made with actions',
-        important: false,
+        type: 'notes/toggleImportanceOf',
         id: 2
       }]
   
