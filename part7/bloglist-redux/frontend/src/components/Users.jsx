08252d1f2d0d5c3b54395439f278a720e1ab0b8c
Mike Vautour <mike@intergalacticlab.com>
2023-03-22 14:35:36 -0400

    7.14 user view 7.15 individual User view

---
+++ part7/bloglist-redux/frontend/src/components/Users.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom"
+
+import { useSelector } from "react-redux"
+
+const Users = () => {
+  const users = useSelector(state => state.users)
+  const userElements = users.map(user => {
+    return (
+      <div className="users--user" key={user.id}>
+        <div className="users--user-name"><Link to={`/users/${user.id}`} >{user.name}</Link></div>
+        <div className="users--user-blogcount">{user.blogs.length}</div>
+      </div>
+    )
+  })
+  return (
+    <section className="users">
+      <div className="users--heading">
+        <h2>Users</h2>
+      </div>
+      <div className="users--stats">
+        <div className="users--user-heading">
+          <div className="users--user-heading-name"></div>
+          <div className="users--user-heading-blogcount">blogs created</div>
+        </div>
+        {userElements}
+      </div>
+    </section>
+  )
+}
+
+export default Users
