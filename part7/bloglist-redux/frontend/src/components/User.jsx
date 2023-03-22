d04b37a781321d8982cd0df8d49b09553febf03a
Mike Vautour <mike@intergalacticlab.com>
2023-03-22 14:42:07 -0400

    changed check for existing state

--- part7/bloglist-redux/frontend/src/components/User.jsx
+++ part7/bloglist-redux/frontend/src/components/User.jsx
@@ -2,15 +2,22 @@ import { useSelector } from "react-redux"
 import { useParams } from "react-router-dom"
 
 const User = () => {
+  
   const { id } = useParams()
+  
   const user = useSelector(state => {
     if (state.users) {
       return state.users.find(user => id === user.id)
     }
   })
-  if (user) {
-    const userBlogs = user.blogs.map(blog => <li key={blog.id}>{`${blog.title}`}</li>)
-    return (
+  
+  if (!user) {
+    return null
+  }
+  
+  const userBlogs = user.blogs.map(blog => <li key={blog.id}>{`${blog.title}`}</li>)
+  
+  return (
     <section className="user">
       <h2>{user.name}</h2>
       <h4> added Blogs </h4>
@@ -18,9 +25,7 @@ const User = () => {
           {userBlogs}
       </ul>
     </section>
-    )
-  }
-  return null
+  )
 }
 
 export default User
--- part7/bloglist-redux/frontend/src/components/Users.jsx
+++ part7/bloglist-redux/frontend/src/components/Users.jsx
@@ -4,32 +4,34 @@ import { useSelector } from "react-redux"
 
 const Users = () => {
   const users = useSelector(state => state.users)
-  if (users) {
-    const userElements = users.map(user => {
-      return (
-          <div className="users--user" key={user.id}>
-            <div className="users--user-name"><Link to={`/users/${user.id}`} >{user.name}</Link></div>
-            <div className="users--user-blogcount">{user.blogs.length}</div>
-          </div>
-      )
-    })
-    
-    return (
-      <section className="users">
-        <div className="users--heading">
-          <h2>Users</h2>
-        </div>
-        <div className="users--stats">
-          <div className="users--user-heading">
-            <div className="users--user-heading-name"></div>
-            <div className="users--user-heading-blogcount">blogs created</div>
-          </div>
-          {userElements}
-        </div>
-      </section>
-    )
+
+  if (!users) {
+    return null
   }
-  return null
+
+  const userElements = users.map(user => {
+    return (
+        <div className="users--user" key={user.id}>
+          <div className="users--user-name"><Link to={`/users/${user.id}`} >{user.name}</Link></div>
+          <div className="users--user-blogcount">{user.blogs.length}</div>
+        </div>
+    )
+  })
+  
+  return (
+    <section className="users">
+      <div className="users--heading">
+        <h2>Users</h2>
+      </div>
+      <div className="users--stats">
+        <div className="users--user-heading">
+          <div className="users--user-heading-name"></div>
+          <div className="users--user-heading-blogcount">blogs created</div>
+        </div>
+        {userElements}
+      </div>
+    </section>
+  )
 }
 
 export default Users
