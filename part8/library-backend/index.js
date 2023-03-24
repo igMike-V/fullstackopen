dc50a1ede0819f7a2a99a5ff28c95a60f61c2817
Mike Vautour <mike@intergalacticlab.com>
2023-03-24 12:22:15 -0400

    8.4 Books of an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -92,7 +92,7 @@ let books = [
 const typeDefs = `
   type Query {
     bookCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
   }
   type Query {
     authorCount: Int!
@@ -114,7 +114,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => authors,
   },
   Author: {
