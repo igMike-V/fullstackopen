2921212bda7131a6fb7a19ecd91c30568a33ac0a
Mike Vautour <mike@intergalacticlab.com>
2023-03-30 11:57:05 -0400

    8.16 listing books

---
+++ part8/library-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+
+const LoginForm = ({ setToken }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [login, result] = useMutation(LOGIN, {
+    onError: (error) => {
+      console.log(error)
+    }
+  })
+
+  useEffect(() => {
+    if (result.data) {
+      const token = result.data.login.value
+      setToken(token)
+      localStorage.setItem('library-user-token', token)
+    }
+  }, [result.date])
+  
+  const submit = async (event) => {
+    event.preventDefault()
+    console.log(username, password)
+    login({ variables: {username, password} })
+  }
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={submit}>
+        <div>
+          username <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password <input
+            type='password'
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginForm
