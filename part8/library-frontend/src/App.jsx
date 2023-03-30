7cf2afd3803a7e20d4767ed5855b2505ddf0c3bb
Mike Vautour <mike@intergalacticlab.com>
2023-03-30 11:57:15 -0400

    8.17 Listing Books

--- part8/library-frontend/src/App.jsx
+++ part8/library-frontend/src/App.jsx
@@ -1,10 +1,37 @@
 import Authors from './components/Authors.jsx'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
 import './App.css'
 import { Routes, Route, Link, Navigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { useApolloClient, useQuery } from '@apollo/client'
+import { useNavigate } from 'react-router-dom'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+  const navigate = useNavigate()
+
+  //set token from storage 
+  useEffect(() => {}, [])
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+    navigate('/')
+  }
+
+
+  if (!token) {
+    return (
+      <>
+        <LoginForm setToken={setToken} />
+      </>
+    )
+  }
+  
 
   return (
     <div className='App'>
@@ -12,13 +39,13 @@ const App = () => {
         <Link className='nav--link' to="/authors" >authors</Link>
         <Link className='nav--link' to="/books" >books</Link>
         <Link className='nav--link' to="/add" >add</Link>
+        <button onClick={logout}>Logout</button>
       </nav>
       <Routes>
         <Route path="/" exact element={<Navigate to="/authors" />} />
         <Route path="/authors" element={<Authors />} >Authors</Route>
         <Route path="/books" element={<Books />} >Books</Route>
         <Route path="/add" element={<NewBook />} >Add</Route>
-        
       </Routes>
     </div>
   )
