22012bbb76bb2511f2d5bc08c88f94cbf9aa8b17
Mike Vautour <mike@intergalacticlab.com>
2023-04-26 14:34:14 -0400

    9.11: Patientor backend, step4

--- part9/patientor/backend/src/types.ts
+++ part9/patientor/backend/src/types.ts
@@ -2,4 +2,15 @@ export interface Diagnose {
   code: string;
   name: string;
   latin?: string;
-} 
+}
+
+export type NonSensitivePatientEntry = Omit<Patient, 'ssn'>;
+
+export interface Patient {
+  id: string;
+  name: string;
+  dateOfBirth: string;
+  ssn: string;
+  gender: string;
+  occupation: string;
+}
